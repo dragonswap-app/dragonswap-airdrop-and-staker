66eb27674faad9c430f766b4ee67976200eb615b
markolazic01 <markolazic01@gmail.com>
2025-06-25 01:15:38 +0200

    Interface update

--- src/interfaces/IStaker.sol
+++ src/interfaces/IStaker.sol
@@ -4,4 +4,5 @@ interface IStaker {
     function stake(address account, uint256 amount, bool locking) external;
     function treasury() external view returns (address);
     function fee() external view returns (uint256);
+    function computeFeeAmount(uint256 amount) external view returns (uint256);
 }
